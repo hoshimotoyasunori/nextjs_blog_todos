2799a418e8af78dfc241d66de513ace973ecde3e
hoshimotoyasunori <hoshimotoyasunori@gmail.com>
2022-06-14 00:00:36 +0900

    add app

--- components/ListData.js
+++ components/ListData.js
@@ -1,42 +1,44 @@
-import React from 'react'
+import React from "react";
 
 export const ListData = [
   {
-    title:"現在のアプリ",
-    url:"https://nextjs-blog-todos-sandy.vercel.app/",
-    skill:"DjangoRestFramework,Next.js,Tailwind",
-    trial:"初めてdjango-restframeworkを使用したので、ユーザー認証においてJWT認証を使用しているが、認証の流れがいまいち理解できていなかったので、調査に時間がかかった。",
-    thumbnail:"/hoshimoto.png",
+    title: "現在のアプリ",
+    url: "https://nextjs-blog-todos-sandy.vercel.app/",
+    skill: "DjangoRestFramework,Next.js,Tailwind",
+    trial:
+      "初めてdjango-restframeworkを使用したので、ユーザー認証においてJWT認証を使用しているが、認証の流れがいまいち理解できていなかったので、調査に時間がかかった。",
+    thumbnail: "/hoshimoto.png",
   },
   {
-    title:"BUILD-ONE",
-    url:"https://oneteam8pos.herokuapp.com/",
-    skill:"Laravel,Bootstrap",
-    trial:"G'sAcademyというプログラミングスクールでの卒業課題として作成したスポーツチームでの情報共有するための画像投稿アプリ。Laravelとdbはmysqlを使用した、シンプルなアプリケーション",
-    thumbnail:"/team.png",
+    title: "BUILD-ONE",
+    url: "https://oneteam8pos.herokuapp.com/",
+    skill: "Laravel,Bootstrap",
+    trial:
+      "G'sAcademyというプログラミングスクールでの卒業課題として作成したスポーツチームでの情報共有するための画像投稿アプリ。Laravelとdbはmysqlを使用した、シンプルなアプリケーション",
+    thumbnail: "/team.png",
   },
   {
-    title:"YUUREA-LP",
-    url:"https://yuurea.com/",
-    skill:"WordPress,GAS",
-    trial:"G'sAcademy卒業後、株式会社ユーリアの会社立ち上げに加担し、作成したLPであり、WordPressを使用し、コンタクトがあれば、GASを使用し、直接Gmailへ送信。かつコンタクト履歴はスプレッドシートに追記していく。",
-    thumbnail:"/yuurea.png",
+    title: "YUUREA-LP",
+    url: "https://yuurea.com/",
+    skill: "WordPress,GAS",
+    trial:
+      "G'sAcademy卒業後、株式会社ユーリアの会社立ち上げに加担し、作成したLPであり、WordPressを使用し、コンタクトがあれば、GASを使用し、直接Gmailへ送信。かつコンタクト履歴はスプレッドシートに追記していく。",
+    thumbnail: "/yuurea.png",
   },
   {
-    title:"Firebase-Line",
-    url:"https://react-line.vercel.app/",
-    skill:"react,firebase",
-    trial:"reactにて、Lineのようなリアルタイムチャットを作成。Googleアカウントにてログインを行い、firebaseを使い、リアルタイムチャット機能を追加し、vercelにてデプロイをした。苦労した点はデプロイ時の環境変数の設定と、firebaseの設定を調べるのに苦労した。",
-    thumbnail:"/firebase_line.png",
+    title: "Firebase-Line",
+    url: "https://react-line.vercel.app/",
+    skill: "react,firebase",
+    trial:
+      "reactにて、Lineのようなリアルタイムチャットを作成。Googleアカウントにてログインを行い、firebaseを使い、リアルタイムチャット機能を追加し、vercelにてデプロイをした。苦労した点はデプロイ時の環境変数の設定と、firebaseの設定を調べるのに苦労した。",
+    thumbnail: "/firebase_line.png",
   },
   {
-    title:"STREAMLIT-APP",
-    url:"/",
-    skill:"python,opencv,作成中",
-    trial:"pythonでのopencvを使用したアプリを作成したいと考えており、骨格検知技術習得のため、面白いものがあったので作成中。",
-    thumbnail:"/",
+    title: "TARGET-APP",
+    url: "https://target-share-app-frontend.vercel.app/",
+    skill: "react(redux),DjangoRestFramework,material-UI",
+    trial:
+      "大谷翔平選手が高校時代に使用したと言われるマンダラチャートを作成し、その他ユーザーの投稿も核部分だけ覗き見ることができるようにした。",
+    thumbnail: "/target-app.png",
   },
-
 ];
-
-
